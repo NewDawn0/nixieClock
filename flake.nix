452644a4f993c483813cab0e60825b9bb1ed86e5
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 15:12:03 +0100

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -1,46 +1,32 @@
 {
   description = "CLI clock display in nixie tubes";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    utils = {
-      url = "github:NewDawn0/nixUtils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { self, nixpkgs, utils, ... }@inputs:
-    let
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ inputs.rust-overlay.overlays.default ];
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      nixie-clock = self.packages.${prev.system}.default;
+    };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "nixie-clock";
+        version = "1.0.0";
+        src = ./.;
+        useFetchCargoVendor = true;
+        cargoHash = "sha256-T+oRN10Nf39AoPHZav0PZVKVU9zBjBpYGNz8PINkGYg=";
+        meta = {
+          description = "CLI clock display in nixie tubes";
+          longDescription = ''
+            A unique command-line clock that displays the current time using Nixie tube-style digits.
+            This charming design adds a vintage touch to your terminal while providing an accurate clock.
+          '';
+          homepage = "https://github.com/NewDawn0/nixieClock";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
         };
-    in {
-      overlays.default = final: prev: {
-        nixie-clock = self.packages.${prev.system}.default;
       };
-      packages = utils.lib.eachSystem { inherit mkPkgs; } (pkgs: {
-        default = pkgs.rustPlatform.buildRustPackage {
-          pname = "nixie-clock";
-          version = "1.0.0";
-          src = ./.;
-          useFetchCargoVendor = true;
-          cargoHash = "sha256-T+oRN10Nf39AoPHZav0PZVKVU9zBjBpYGNz8PINkGYg=";
-          meta = {
-            description = "CLI clock display in nixie tubes";
-            longDescription = ''
-              A unique command-line clock that displays the current time using Nixie tube-style digits.
-              This charming design adds a vintage touch to your terminal while providing an accurate clock.
-            '';
-            homepage = "https://github.com/NewDawn0/nixieClock";
-            license = pkgs.lib.licenses.mit;
-            maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-            platforms = pkgs.lib.platforms.all;
-          };
-        };
 
-      });
-    };
+    });
+  };
 }
